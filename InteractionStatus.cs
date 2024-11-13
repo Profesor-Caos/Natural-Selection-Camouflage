a4f80361bf1d1eb14f82155bfc83a3769a10cb22
Ryan Gole <ryan.m.gole@gmail.com>
2024-11-13 17:03:11 -0500

    Implementing resizing with web export included

--- InteractionStatus.cs
+++ InteractionStatus.cs
@@ -21,9 +21,6 @@ namespace NaturalSelectionCamouflage
             if (IsControlGroup)
                 return true;
 
-            if (PageNumber == 0)
-                return false;
-
             if (PageNumber != 3)
                 return true;
 
--- Main.cs
+++ Main.cs
@@ -5,6 +5,8 @@ using System.Collections.Generic;
 public class Main : HBoxContainer
 {
     public Queue<string> Logs = new Queue<string>();
+    private static float PROJECT_WIDTH = 1620.0f;
+    private static float PROJECT_HEIGHT = 600.0f;
 
     private void OnNavigationPageNavigationButtonPressed()
     {
@@ -13,15 +15,26 @@ public class Main : HBoxContainer
         InteractionStatus.PageNumber = pageNumber;
     }
 
+    private void ResizeUI()
+    {
+        // Get the current window size
+        Vector2 windowSize = GetViewport().Size;
+
+        // Scale the root Control node based on window size
+        // You can adjust these values to suit your design needs
+        float scaleFactor = Mathf.Min(windowSize.x / PROJECT_WIDTH, windowSize.y / PROJECT_HEIGHT);
+        RectScale = new Vector2(scaleFactor, scaleFactor);
+    }
+
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
     {
-        
+        ResizeUI();
     }
 
-//  // Called every frame. 'delta' is the elapsed time since the previous frame.
-//  public override void _Process(float delta)
-//  {
-//      
-//  }
+    //  // Called every frame. 'delta' is the elapsed time since the previous frame.
+    public override void _Process(float delta)
+    {
+        ResizeUI();
+    }
 }
